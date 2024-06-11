e6e634071d21a5bd6625d486b8512e972faaa241
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-06-11 13:00:31 +0700

    feat: add absolute import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import ExampleComponent from "@/components/Example";
 function App() {
   return (
     <>
+      <ExampleComponent />
       <h1 className="bg-blue-500">Test</h1>
     </>
   );
---
+++ src/components/Example.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+interface IExampleComponentProps {}
+
+const ExampleComponent: React.FunctionComponent<
+  IExampleComponentProps
+> = () => {
+  return (
+    <section>
+      <h1>TEST</h1>
+    </section>
+  );
+};
+
+export default ExampleComponent;
